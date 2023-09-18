c215d022e407491d739cc02fb7842883322e67da
EhlakovaNatalia <ehlakovanatalia@gmail.com>
2023-09-18 11:45:16 +0400

    first commit preloader

--- async-requests/preloader/task.js
+++ async-requests/preloader/task.js
@@ -1,40 +1,35 @@
-const poolAnswers = document.getElementById('poll__answers');
 const xhr = new XMLHttpRequest();
-xhr.open('GET', 'https://students.netoservices.ru/nestjs-backend/poll');
-xhr.addEventListener('load', function() {
+xhr.open('GET', 'https://students.netoservices.ru/nestjs-backend/slow-get-courses');
+  xhr.onload = function() {
     if(xhr.readyState === xhr.DONE) {
         if(xhr.status >= 400) {
             setTimeout(() => {
-                return alert('Ошибка загрузки данных о вопросе');
+                return alert('Ошибка загрузки данных о курсе валют');
             }, 1000)
         }
+
         if(xhr.status === 200) {
             const response = JSON.parse(xhr.responseText);
-            const poolTitleText = response.data.title;
-            const poolAnswersText = response.data.answers;
-
-            const poolTitle = document.getElementById('poll__title');
-            poolTitle.textContent = poolTitleText;
-
-            poolAnswersText.forEach(answer => {
-                createHTML(answer);
-            });
-
-            const btnAnswers = document.querySelectorAll('.poll__answer');
-            btnAnswers.forEach(btn => {
-                btn.addEventListener('click', () => {
-                    alert('Спасибо, Ваш голос засчитан!');
-                    
-                })
-            })
+            const valutes = response.response.Valute;
+            console.log(valutes)
+            for (const valute in valutes) {
+              const value = Number(valutes[valute].Value).toFixed(2);
+              const item = createHTML(valute, value);
+              items.appendChild(item);
+            }
+            imgLoader.classList.remove('loader_active');
         }
-    }
-})
+    };
+};
 xhr.send();
-function createHTML(answer) { 
-    const btnAnswer = document.createElement('button');
-    btnAnswer.classList.add('poll__answer');
-    btnAnswer.textContent = answer;
-    poolAnswers.appendChild(btnAnswer);
-    return btnAnswer;
+function createHTML(valute, value) {
+    const item = document.createElement('div');
+    item.classList.add('item');
+
+    item.innerHTML += `
+        <div class="item__code">${valute}</div>
+        <div class="item__value">${value}</div>
+        <div class="item__currency">руб.</div>
+    `
+    return item;
 }
