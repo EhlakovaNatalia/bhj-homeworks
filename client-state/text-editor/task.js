e99453b1e938e531029f858398efb172db80ed49
EhlakovaNatalia <ehlakovanatalia@gmail.com>
2023-09-18 11:56:17 +0400

    first commit auth

--- client-state/text-editor/task.js
+++ client-state/text-editor/task.js
@@ -3,7 +3,6 @@ const textEditor = document.getElementById('editor');
 if(storedText !== null) {
     textEditor.value = storedText;
 }
-
 textEditor.addEventListener('input', () => {
     localStorage.setItem('text', textEditor.value);
 })
